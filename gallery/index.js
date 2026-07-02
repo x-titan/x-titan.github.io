66a699e7e40705576928a03ab753a3461094890a
Aset Telmanov <telmanov2002.at@gmail.com>
2026-07-03 00:24:17 +0500

    Remove unused files and styles, add new layout and component styles, and update settings for improved design consistency

--- gallery/index.js
+++
@@ -1,3 +0,0 @@
-import { is, each, range, validate } from "https://x-titan.github.io/utils/index.js"
-import { search, css, validHTML, isHTML, attr, } from "https://x-titan.github.io/web-utils/index.js"
-
